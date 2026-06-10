8fd80dc26e186dc4ad41c32c13b5844cf4f7337a
Медведев Сергей <c0nterdash@gmail.com>
2026-06-10 21:54:35 +0500

    server working

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const app = require('./app');
+const pool = require('./db/pool');
+
+// pool.query('SELECT NOW', (err, res) => {
+//     if (err) { console.log('Error connecting to the database', err.stack);  }
+//     else { console.log('Database connect is succesfuly.', res.rows); }
+// });
+
+const PORT = process.env.PORT || 4000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Сервер слушает порт: ${PORT}`);
+})
